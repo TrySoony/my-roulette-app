c9d19a397628453b88d1878177e24d189110c1b0
TrySoony <125113485+TrySoony@users.noreply.github.com>
2025-06-20 22:32:38 +0300

    Update prizes.js

--- prizes.js
+++ prizes.js
@@ -1,11 +1,43 @@
 // prizes.js
 const prizes = [
-  { name: "iPhone 15", price: 90000 },
-  { name: "AirPods", price: 15000 },
-  { name: "1000₽", price: 1000 },
-  { name: "Пусто", price: 0 },
-  { name: "MacBook", price: 150000 },
-  { name: "Чашка", price: 500 },
-  { name: "PlayStation 5", price: 60000 },
-  { name: "Книга", price: 1000 }
+  { 
+    name: "Лампочка", 
+    starPrice: 1, 
+    img: "https://telegram.org/file/464001154/1/b7G2O2f2m4s.30792/b8a4ca3c224c7a6f3b" 
+  },
+  { 
+    name: "Хот-дог", 
+    starPrice: 5, 
+    img: "https://telegram.org/file/464001154/1/7l_QzBqpdks.33796/c234b6fdf815525048" 
+  },
+  { 
+    name: "НЛО", 
+    starPrice: 10, 
+    img: "https://telegram.org/file/464001154/1/b5WbZ2z3G8E.33810/a56396e8e50f55e347" 
+  },
+  { 
+    name: "Дракон", 
+    starPrice: 25, 
+    img: "https://telegram.org/file/464001154/1/b28dff4e2d3.33842/b4d75f284d72863f68" 
+  },
+  { 
+    name: "Кот-учёный", 
+    starPrice: 50, 
+    img: "https://telegram.org/file/464001154/1/d798b0f8cab.33857/d151d932145b46f56a" 
+  },
+  { 
+    name: "Money Gun", 
+    starPrice: 100, 
+    img: "https://telegram.org/file/464001154/1/b-Ww4M6a8M4.33890/c8541c8f15be7764d8" 
+  },
+  { 
+    name: "Lamborghini", 
+    starPrice: 500, 
+    img: "https://telegram.org/file/464001154/1/8PjO0kSVeDo.33924/3169f4851bb8d9f485" 
+  },
+  { 
+    name: "Пусто", 
+    starPrice: 0, 
+    img: "" // Пустая строка для пустого приза
+  }
 ];
