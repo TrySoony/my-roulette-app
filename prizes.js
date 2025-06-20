6983b6a2f16b8465417838b3597330af13e9cf38
TrySoony <125113485+TrySoony@users.noreply.github.com>
2025-06-20 21:33:29 +0300

    Add files via upload

---
+++ prizes.js
@@ -0,0 +1,11 @@
+// prizes.js
+const prizes = [
+  { name: "iPhone 15", price: 90000 },
+  { name: "AirPods", price: 15000 },
+  { name: "1000₽", price: 1000 },
+  { name: "Пусто", price: 0 },
+  { name: "MacBook", price: 150000 },
+  { name: "Чашка", price: 500 },
+  { name: "PlayStation 5", price: 60000 },
+  { name: "Книга", price: 1000 }
+];
