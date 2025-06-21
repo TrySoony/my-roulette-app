33314d31a41465b7f88e2b206e246f860b516b46
TrySoony <125113485+TrySoony@users.noreply.github.com>
2025-06-21 09:15:49 +0300

    Update prizes.js

--- prizes.js
+++ prizes.js
@@ -1,39 +1,19 @@
 // prizes.js
 const prizes = [
   { 
-    name: "Лампочка", 
-    starPrice: 1, 
-    img: "https://telegram.org/file/464001154/1/b7G2O2f2m4s.30792/b8a4ca3c224c7a6f3b" 
+    name: "Nail Bracelet", 
+    starPrice: 100000, 
+    img: "https://nft.telegram.org/media/bracelet_red_fang_blue_viper.tgs" 
   },
   { 
-    name: "Хот-дог", 
-    starPrice: 5, 
-    img: "https://telegram.org/file/464001154/1/7l_QzBqpdks.33796/c234b6fdf815525048" 
+    name: "Bonded Ring", 
+    starPrice: 37500, 
+    img: "https://nft.telegram.org/media/ring_miami_cobalt_dragon.tgs" 
   },
   { 
-    name: "НЛО", 
-    starPrice: 10, 
-    img: "https://telegram.org/file/464001154/1/b5WbZ2z3G8E.33810/a56396e8e50f55e347" 
-  },
-  { 
-    name: "Дракон", 
-    starPrice: 25, 
-    img: "https://telegram.org/file/464001154/1/b28dff4e2d3.33842/b4d75f284d72863f68" 
-  },
-  { 
-    name: "Кот-учёный", 
-    starPrice: 50, 
-    img: "https://telegram.org/file/464001154/1/d798b0f8cab.33857/d151d932145b46f56a" 
-  },
-  { 
-    name: "Money Gun", 
-    starPrice: 100, 
-    img: "https://telegram.org/file/464001154/1/b-Ww4M6a8M4.33890/c8541c8f15be7764d8" 
-  },
-  { 
-    name: "Lamborghini", 
-    starPrice: 500, 
-    img: "https://telegram.org/file/464001154/1/8PjO0kSVeDo.33924/3169f4851bb8d9f485" 
+    name: "Neko Helmet", 
+    starPrice: 14000, 
+    img: "https://nft.telegram.org/media/helmet_twister_chestnut_candy.tgs" 
   },
   { 
     name: "Пусто", 
