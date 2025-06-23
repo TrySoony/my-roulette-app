6867a1b362db52c5e21c1066ede23902ef72ee41
TrySoony <125113485+TrySoony@users.noreply.github.com>
2025-06-23 10:53:24 +0300

    Delete app.py

--- app.py
+++
@@ -1,7 +0,0 @@
-# Импортируем FastAPI приложение из main.py
-from main import app
-
-# Экспортируем приложение для Gunicorn
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000) 
